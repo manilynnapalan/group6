bb405f3783529a3a7fee009d12e172d14e1deddc
manilynnapalan <napalanmanilyn73@gmail.com>
2022-04-21 21:27:57 -0700

    added homepage

--- index.php
+++ index.php
@@ -2,9 +2,42 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Sample</title>
+	<title>
+		Airline Booking web page
+	</title>
 </head>
-<body> background" 
-	<h2>Welcome</h2>
+<body background="1.jpg">
+	<br />
+   <h3 align="center">
+   	   <front face="Lato" size="6">LOGO</front>
+   	   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   	   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   	   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   	   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   	   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   	   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   	   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   	   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   	   <front face="cinzel" size="4">
+   	   	    <a href="#">HOME</a>&nbsp;&nbsp;&nbsp;&nbsp;
+   	   	    <a href="#">VIDEOS</a>&nbsp;&nbsp;&nbsp;&nbsp;
+   	   	    <a href="#">PORTFOLIO</a>&nbsp;&nbsp;&nbsp;&nbsp;
+   	   	    <a href="#">BLOG</a>&nbsp;&nbsp;&nbsp;&nbsp;
+   	   	    <a href="#">CONTACT US</a>
+   	   	</front>
+   	</h3>
+   	<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+   	<h1 align="center">
+   		<font face="Lato" color="#017bf5">
+   			AIRLINE BOOKING WEB PAGE
+   		</font>
+   	</h1>
+   	<br />
+   	<h3 align="center">
+   	<a href="#">
+   		<font face="Lato" color="#000">GET STARTED</font>
+   	</a>&nbsp;&nbsp;&nbsp;&nbsp;
+   	<a href="#">
+   	<font face="Lato" color="#fff">SUBSCRIBE US</font>
 </body>
 </html>
